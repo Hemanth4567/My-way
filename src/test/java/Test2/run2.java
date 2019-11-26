e5375ee15a1f72122b3640fb2c6f261dfdf26890
Training_b6b.00.07 <Training_b6b.00.07@BDC6-DX-87Q4N62.dir.svc.accenture.com>
2019-11-26 12:56:25 +0530

    gt

--- src/test/java/Test2/run2.java
+++ src/test/java/Test2/run2.java
@@ -7,7 +7,7 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		plugin= {"json:src/test/java/Report/output/jason.json"})
+		plugin= {"json:src/test/java/Test2/output/jason.json"})
 public class run2 {
 	
 
